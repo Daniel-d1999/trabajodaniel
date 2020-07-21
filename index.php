045d7e885c80c84ef5f95f50d6b0666ed24e1b32
daniel <ortizroman123456789@gmail.com>
2020-07-21 16:57:24 -0500

    esto es el index

--- index.php
+++ index.php
@@ -7,4 +7,39 @@ return $a+$b;
  }
  
  ?>
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>daniel ortiz </title>
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    
+    <link rel="stylesheet" href="icon/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    
+</head>
+<body>
+    
+<div class="container-form">
+        <div class="header">
+            <div class="logo-title">
+                <img src="image/logo_magtimus.png" alt="">
+                <h2>daniel ortiz roman</h2>
+            </div>
+            <div class="menu">
+                <a href="logearse.php"><li class="module-login active">logearse</li></a>
+                <a href="registrar.php"><li class="module-register">registrarse</li></a>
+         
+            
+            </div>
+        
+            
+                
+        </form>
+    </div>
+    
+</body>
+</html>
  
